7fc7e92cb86c9f9e487e0f4df90ba3ad7ab159d2
风中的芦苇哥 <1157880324@qq.com>
2019-05-08 14:32:28 +0800

    fix

--- iphoneXDemo/ViewController.m
+++ iphoneXDemo/ViewController.m
@@ -7,6 +7,18 @@
 //
 
 #import "ViewController.h"
+#import <sys/sysctl.h>
+#import <sys/utsname.h>
+
+static inline BOOL isIPhoneXSeries() {
+    if (@available(iOS 11.0, *)) {
+        UIWindow *mainWindow = [[[UIApplication sharedApplication] delegate] window];
+        if (mainWindow.safeAreaInsets.bottom > 0.0) {
+            return YES;
+        }
+    }
+    return NO;
+}
 
 //一.iPhone X尺寸问题
 //
@@ -52,6 +64,24 @@
 
 #define IS_iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
 
+//判断是否是刘海屏
+#define IPHONE_X \
+({BOOL isPhoneX = NO;\
+if (@available(iOS 11.0, *)) {\
+isPhoneX = [[UIApplication sharedApplication] delegate].window.safeAreaInsets.bottom > 0.0;\
+}\
+(isPhoneX);})
+
+
+//iPhoneXr 下面的判断会失效 估计是官方的问题
+
+#define iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO) //判断iPhoneXs
+#define iPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) : NO)
+#define iPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) : NO)
+#define iPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1080, 1920), [[UIScreen mainScreen] currentMode].size) : NO)
+#define iPhoneXr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) : NO)
+#define iPhoneXsMax ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) : NO)
+
 
 @interface ViewController ()
 
@@ -72,6 +102,34 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+/**
+ 判断iPhoneXr硬件编码/标识符iPhone11,8
+ 
+ @return return value description
+ */
+- (BOOL)isiPhoneXrDevice {
+    static BOOL isiPhoneXr = NO;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+#if TARGET_IPHONE_SIMULATOR
+        // 获取模拟器所对应的 device model
+        NSString *model = NSProcessInfo.processInfo.environment[@"SIMULATOR_MODEL_IDENTIFIER"];
+#else
+        // 获取真机设备的 device model
+        struct utsname systemInfo;
+        uname(&systemInfo);
+        NSString *model = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+#endif
+        // 判断 device model 是否为 "iPhone11,8"
+        // 如果是，就认为是 iPhone Xr
+        isiPhoneXr = [model isEqualToString:@"iPhone11,8"];
+        //        NSLog(@"-------------%@-----------",model);
+    });
+    
+    return isiPhoneXr;
 }
 
 
